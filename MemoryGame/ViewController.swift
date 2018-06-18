95107c4f52d69e46fcc5cd9898706dbd8b9d4878
Paolo Braccini New <paolo_braccini@macbook-pro.local>
2018-06-18 19:13:44 +0200

    first

--- MemoryGame/ViewController.swift
+++ MemoryGame/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var emoji = ["👽","🐹","🐷"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet var allButtons: [UIButton]!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func touchedButton(_ sender: Any) {
+       let abutton = sender as! UIButton
+       let indice = allButtons.index(of: abutton)
+        if abutton.title(for: .normal) == "" {
+            abutton.backgroundColor = .white
+            abutton.setTitle(emoji[indice!], for: .normal)
+            
+            abutton.layer.transform = CATransform3DConcat(abutton.layer.transform, CATransform3DMakeRotation(CGFloat(M_PI),1.0,0.0,0.0))
+        } else {
+            abutton.backgroundColor = .orange
+            abutton.setTitle("", for: .normal)
+            abutton.layer.transform = CATransform3DConcat(abutton.layer.transform, CATransform3DMakeRotation(CGFloat(M_PI),1.0,0.0,0.0))
+        }
+    }
+    
 
 }
 
